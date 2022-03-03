bf9e7e2a11ec77db798627f615f0ebc415a6a855
Mads Horsted Brunebjerg <mads81i7@edu.easj.dk>
2022-03-03 10:26:10 +0100

    Add project files.

---
+++ CrackerServerNet5/CrackerTCPServer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AbstractTCPServerClassLibrary.TCPServer;
+using CrackerServerNet5.util;
+using PasswordCrackerCentralized.model;
+
+namespace CrackerServerNet5
+{
+    class CrackerTCPServer : AbstractTCPServer
+    {
+        private int _chunkSize = 5000;
+        private int _nextClientStartpoint = 0;
+
+        public CrackerTCPServer(int port, string name, int shutDownPort, string debugLevel) : base(port, name, shutDownPort, debugLevel)
+        {
+
+        }
+
+        public override void TcpServerWork(StreamReader reader, StreamWriter writer)
+        {
+            BlockingCollection<List<string>> chunks = new BlockingCollection<List<string>>();
+            List<UserInfo> users = new List<UserInfo>();
+            List<UserInfoClearText> results = new List<UserInfoClearText>();
+
+            using (FileStream fs = new FileStream("webster-dictionary.txt", FileMode.Open, FileAccess.Read))
+
+            using (StreamReader dictionary = new StreamReader(fs))
+            {
+                var n = 0;
+                while (!dictionary.EndOfStream)
+                {
+                    if (n % _chunkSize == 0)
+                    {
+                        chunks.Add(new List<string>());
+                    }
+                    string dictionaryEntry = dictionary.ReadLine();
+                    chunks.Last().Add(dictionaryEntry);
+                    n++;
+                }
+            }
+
+            users = PasswordFileHandler.ReadPasswordFile("passwords.txt");
+
+            string line = reader.ReadLine();
+
+            switch (line.ToLower())
+            {
+                case "passwords":
+                    break;
+                case "nextchunk":
+                    break;
+                case "finished":
+                    break;
+                default:
+                    break;
+            }
+
+            writer.Flush();
+        }
+    }
+}
---
+++ CrackerServerNet5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CrackerServerNet5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
